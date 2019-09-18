8e8095f0067019679534b6024057d5bab473c0f4
kawhiLin <17816879402@163.com>
2019-09-18 10:07:23 +0800

    添加web-en

--- web-en/start.sh
+++ web-en/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash 
-
+  
 CMDVAR="-Dcse.service.registry.address=https://$CONFIGIP:30100 -Dcse.config.client.serverUri=https://$CONFIGIP:30103 -Dcse.monitor.client.serverUri=https://$CONFIGIP:30109 -Dcse.credentials.accessKey=$AK -Dcse.credentials.secretKey=$SK -Dcse.credentials.project=$PROJECT" 
-
-trap 'ps -ax | grep -w record |grep -v grep |cut -c 1-6 |xargs kill -15;sleep 5s '  SIGTERM
+trap 'ps -ax | grep -w web-en |grep -v grep |cut -c 1-6 |xargs kill -15;sleep 5s '  SIGTERM
 nohup java $CMDVAR -jar ./*.jar 2>&1 &
 wait $!
